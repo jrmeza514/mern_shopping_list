773bc30887fa1483758056e40eee12f04e540cdf
Juan Meza <jrmeza514@gmail.com>
2021-06-26 22:58:30 -0700

    Aligned Create List Button

--- client/src/components/CreateListModal.tsx
+++ client/src/components/CreateListModal.tsx
@@ -22,8 +22,7 @@ const  CreateListModal = ({addList}: IListModal) => {
 
     return (
         <>
-            <br/>
-            <Button variant="contained" color="secondary" onClick={toggle}>Create New List</Button>
+            <Button variant="contained" color="secondary" onClick={toggle} className="create-list-btn">Create New List</Button>
             <AppModal open={modal} toggle={toggle}>
                 <h3>Create new list. </h3>
                 <form onSubmit={e => handleOnSubmit(e)}> 
