ed0467b591f4738a22c73ea35f5144f1d81258eb
Juan Meza <jrmeza514@gmail.com>
2021-06-27 23:24:45 -0700

    Implemented Self Sorting Shopping List
    The Shopping Lists will now sort themselves into the column with the least cumulative height.

--- client/src/components/shopping/ShoppingList.tsx
+++ client/src/components/shopping/ShoppingList.tsx
@@ -9,6 +9,20 @@ import CreateListModal from './CreateListModal';
 import ShoppingListHeader from './ShoppingListHeader';
 import ShoppingListBody from './ShoppingListBody';
 
+const getIndexOfSmallestColumn = (cols: number[]): number => {
+    if (cols.length === 0) return 0;
+    if (cols.length === 1) return 0;
+    let indexOfSmallets = 0;
+    let smallest = 10000;
+    for (let i = 0; i < cols.length; i++) {
+        if (cols[i] < smallest) {
+            smallest = cols[i];
+            indexOfSmallets = i;
+        }
+    }
+    return indexOfSmallets;
+}
+
 const ShoppingList = ({ shoppingList, isAuthenticated, getLists, deleteList, deleteItem }: IShoppingList) => {
     const { lists } = shoppingList;
     useEffect(() => {
@@ -22,25 +36,74 @@ const ShoppingList = ({ shoppingList, isAuthenticated, getLists, deleteList, del
         setItemModalOpen(!itemModalOpen);
     };
 
-    if (!isAuthenticated) return null;
-
     const addToList = (title: string, _id: string) => {
         setListId(_id);
         setlistTitle(title);
         setItemModalOpen(true);
     }
 
+    const PADDING = 40;
+    const SPACE_BETWEEN_ITEMTS = 10;
+    const ITEM_HEIGTH_MULTIPLIER = 60;
+    const STATIC_HEIGHT_MODIFIER = 75;
+
+    let TARGET_WIDTH = 350;
+    let VIEWPORT_WIDTH = window.innerWidth - PADDING;
+    if (TARGET_WIDTH >= VIEWPORT_WIDTH) TARGET_WIDTH = VIEWPORT_WIDTH;
+    let numberOfColums = Math.floor(VIEWPORT_WIDTH / TARGET_WIDTH);
+    let columns = new Array(numberOfColums).fill(0);
+    let COLUMN_WIDTH = numberOfColums === 1 ? VIEWPORT_WIDTH :
+        (VIEWPORT_WIDTH / numberOfColums) - ((SPACE_BETWEEN_ITEMTS * (numberOfColums)) / (numberOfColums - 1));
+    const [cardWidth, setCardWidth] = useState(COLUMN_WIDTH);
+
+    const runSizingCalculations = () => {
+        if (TARGET_WIDTH >= VIEWPORT_WIDTH) {
+            TARGET_WIDTH = VIEWPORT_WIDTH
+        }
+        VIEWPORT_WIDTH = window.innerWidth - PADDING;
+        numberOfColums = Math.floor(VIEWPORT_WIDTH / TARGET_WIDTH);
+        columns = new Array(numberOfColums).fill(0);
+        COLUMN_WIDTH = numberOfColums === 1 ? VIEWPORT_WIDTH :
+            (VIEWPORT_WIDTH / numberOfColums) - ((SPACE_BETWEEN_ITEMTS * numberOfColums) / (numberOfColums - 1));
+        setCardWidth(
+            COLUMN_WIDTH
+        );
+    }
+
+    if (!isAuthenticated) return null;
+
+    window.onresize = () => {
+        runSizingCalculations();
+    };
+
+
+
     return (
         <>
             <CreateListModal />
             <ItemModal open={itemModalOpen} toggle={toggle} listId={listId} listTitle={listTitle} />
             <div className="sl-card-container">
-                {lists.map(({ _id, title, items }) => (
-                    <Card key={_id} className="card">
-                        <ShoppingListHeader title={title} _id={_id} addToList={addToList}></ShoppingListHeader>
-                        <ShoppingListBody items={items} deleteItem={deleteItem} listId={_id} />
-                    </Card>
-                ))}
+                {lists.map(({ _id, title, items }) => {
+                    const colIndex = getIndexOfSmallestColumn(columns);
+                    let transYVal = columns[colIndex];
+                    let transXVal = (colIndex * cardWidth) + SPACE_BETWEEN_ITEMTS * colIndex;
+
+                    columns[colIndex] += items.length * ITEM_HEIGTH_MULTIPLIER + STATIC_HEIGHT_MODIFIER;
+
+
+                    return (
+                        <Card key={_id} className="card" style={{
+                            width: cardWidth,
+                            transform: `
+                            translateX(${transXVal}px)
+                            translateY(${transYVal}px)
+                            `
+                        }}>
+                            <ShoppingListHeader title={title} _id={_id} addToList={addToList}></ShoppingListHeader>
+                            <ShoppingListBody items={items} deleteItem={deleteItem} listId={_id} />
+                        </Card>
+                    )
+                })}
             </div>
         </>
     )
