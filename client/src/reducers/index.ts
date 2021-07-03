f0d9d13f31ee9644d72b068b46d7d2054d72d71d
Juan Meza <jrmeza514@gmail.com>
2021-07-03 15:58:41 -0700

    Removed theme reducer
    Will track theme state through through user preferences

--- client/src/reducers/index.ts
+++ client/src/reducers/index.ts
@@ -2,11 +2,9 @@ import { combineReducers } from "redux";
 import errorReducer from './errorReducer';
 import authReducer from './authReducer';
 import listReducer from './listReducer';
-import themeReducer from './themeReducer';
 
 export default combineReducers({
     shoppingList: listReducer,
     auth: authReducer,
-    error: errorReducer,
-    theme: themeReducer
+    error: errorReducer
 })
