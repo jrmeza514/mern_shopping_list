5da6b84a5baf6760cbdfcb72d8ad3227b0317904
Juan Meza <jrmeza514@gmail.com>
2021-07-04 00:31:07 -0700

    Refactoring
    Cleaned up resizing process and made calculation pixel perfect.

--- client/src/components/shopping/ShoppingList.tsx
+++ client/src/components/shopping/ShoppingList.tsx
@@ -1,13 +1,18 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useLayoutEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import ItemModal from './ItemModal';
 import { IExistingList, IListReduxProps, IShoppingList } from '../../types/interfaces';
 import { Card } from '@material-ui/core';
-import { getLists, deleteList } from '../../actions/listActions';
+import { getLists } from '../../actions/listActions';
 import { deleteItem } from '../../actions/ItemActions';
 import CreateListModal from './CreateListModal';
 import ShoppingListHeader from './ShoppingListHeader';
 import ShoppingListBody from './ShoppingListBody';
+
+const PADDING = 20;
+const SPACE_BETWEEN_ITEMTS = 10;
+const ITEM_HEIGTH_MULTIPLIER = 60;
+const STATIC_HEIGHT_MODIFIER = 75;
 
 const getIndexOfSmallestColumn = (cols: number[]): number => {
     if (cols.length === 0) return 0;
@@ -23,8 +28,9 @@ const getIndexOfSmallestColumn = (cols: number[]): number => {
     return indexOfSmallets;
 }
 
-const ShoppingList = ({ shoppingList, isAuthenticated, getLists, deleteList, deleteItem }: IShoppingList) => {
+const ShoppingList = ({ shoppingList, isAuthenticated, getLists, deleteItem }: IShoppingList) => {
     const { lists } = shoppingList;
+
     useEffect(() => {
         if (isAuthenticated) getLists();
     }, [getLists, isAuthenticated])
@@ -42,39 +48,39 @@ const ShoppingList = ({ shoppingList, isAuthenticated, getLists, deleteList, del
         setItemModalOpen(true);
     }
 
-    const PADDING = 40;
-    const SPACE_BETWEEN_ITEMTS = 10;
-    const ITEM_HEIGTH_MULTIPLIER = 60;
-    const STATIC_HEIGHT_MODIFIER = 75;
-
     let TARGET_WIDTH = 350;
-    let VIEWPORT_WIDTH = window.innerWidth - PADDING;
-    if (TARGET_WIDTH >= VIEWPORT_WIDTH) TARGET_WIDTH = VIEWPORT_WIDTH;
-    let numberOfColums = Math.floor(VIEWPORT_WIDTH / TARGET_WIDTH);
-    let columns = new Array(numberOfColums).fill(0);
-    let COLUMN_WIDTH = numberOfColums === 1 ? VIEWPORT_WIDTH :
-        (VIEWPORT_WIDTH / numberOfColums) - ((SPACE_BETWEEN_ITEMTS * (numberOfColums)) / (numberOfColums - 1));
-    const [cardWidth, setCardWidth] = useState(COLUMN_WIDTH);
+    const [cardWidth, setCardWidth] = useState(TARGET_WIDTH);
+
 
     const runSizingCalculations = () => {
-        if (TARGET_WIDTH >= VIEWPORT_WIDTH) {
-            TARGET_WIDTH = VIEWPORT_WIDTH
-        }
-        VIEWPORT_WIDTH = window.innerWidth - PADDING;
-        numberOfColums = Math.floor(VIEWPORT_WIDTH / TARGET_WIDTH);
-        columns = new Array(numberOfColums).fill(0);
-        COLUMN_WIDTH = numberOfColums === 1 ? VIEWPORT_WIDTH :
-            (VIEWPORT_WIDTH / numberOfColums) - ((SPACE_BETWEEN_ITEMTS * numberOfColums) / (numberOfColums - 1));
+        const VIEWPORT_WIDTH = window.innerWidth - PADDING * 2;
+        const ACTUAL_WIDRH = TARGET_WIDTH >= VIEWPORT_WIDTH ? VIEWPORT_WIDTH : TARGET_WIDTH;
+        const numberOfColums = Math.floor((VIEWPORT_WIDTH) / ACTUAL_WIDRH);
+        console.log(numberOfColums);
+
+        const COLUMN_WIDTH = numberOfColums === 1 ? VIEWPORT_WIDTH :
+            (VIEWPORT_WIDTH / numberOfColums) -
+            // Sum of all whitespace divided by number of columns
+            (
+                // White Space Between Cards
+                ((SPACE_BETWEEN_ITEMTS) * (numberOfColums - 1))
+            ) / numberOfColums
+
         setCardWidth(
-            COLUMN_WIDTH
+            Math.floor(COLUMN_WIDTH)
         );
     }
 
+    useLayoutEffect(() => {
+        window.addEventListener('resize', runSizingCalculations);
+        runSizingCalculations();
+        return () => window.removeEventListener('resize', runSizingCalculations);
+    });
+
     if (!isAuthenticated) return null;
 
-    window.onresize = () => {
-        runSizingCalculations();
-    };
+
+
 
 
 
@@ -84,25 +90,28 @@ const ShoppingList = ({ shoppingList, isAuthenticated, getLists, deleteList, del
             <ItemModal open={itemModalOpen} toggle={toggle} listId={listId} listTitle={listTitle} />
             <div className="sl-card-container">
                 {
-                    lists.sort((a: IExistingList, b: IExistingList) => { return b.items.length - a.items.length })
-                        .map(({ _id, title, items }) => {
-                            const colIndex = getIndexOfSmallestColumn(columns);
-                            let transYVal = columns[colIndex];
-                            let transXVal = (colIndex * cardWidth) + SPACE_BETWEEN_ITEMTS * colIndex;
+                    (() => {
+                        const VW_WIDTH = (window.innerWidth - PADDING * 2);
+                        const columns = new Array(Math.floor(VW_WIDTH / TARGET_WIDTH)).fill(0);
+                        return lists.sort((a: IExistingList, b: IExistingList) => { return b.items.length - a.items.length })
+                            .map(({ _id, title, items }) => {
+                                const colIndex = getIndexOfSmallestColumn(columns);
+                                let transYVal = columns[colIndex];
+                                let transXVal = (colIndex * cardWidth) + SPACE_BETWEEN_ITEMTS * colIndex;
+                                columns[colIndex] += items.length * ITEM_HEIGTH_MULTIPLIER + STATIC_HEIGHT_MODIFIER;
 
-                            columns[colIndex] += items.length * ITEM_HEIGTH_MULTIPLIER + STATIC_HEIGHT_MODIFIER;
-
-
-                            return (
-                                <Card key={_id} className="card" style={{
-                                    width: cardWidth,
-                                    transform: `translateX(${transXVal}px) translateY(${transYVal}px)`
-                                }}>
-                                    <ShoppingListHeader title={title} _id={_id} addToList={addToList}></ShoppingListHeader>
-                                    <ShoppingListBody items={items} deleteItem={deleteItem} listId={_id} />
-                                </Card>
-                            )
-                        })}
+                                return (
+                                    <Card key={_id} className="card" style={{
+                                        width: cardWidth,
+                                        transform: `translateX(${transXVal}px) translateY(${transYVal}px)`
+                                    }}>
+                                        <ShoppingListHeader title={title} _id={_id} addToList={addToList}></ShoppingListHeader>
+                                        <ShoppingListBody items={items} deleteItem={deleteItem} listId={_id} />
+                                    </Card>
+                                )
+                            })
+                    })()
+                }
             </div>
         </>
     )
@@ -114,4 +123,4 @@ const mapStateToProps = (state: IListReduxProps) => ({
     isAuthenticated: state.auth.isAuthenticated
 })
 
-export default connect(mapStateToProps, { getLists, deleteList, deleteItem })(ShoppingList);
+export default connect(mapStateToProps, { getLists, deleteItem })(ShoppingList);
