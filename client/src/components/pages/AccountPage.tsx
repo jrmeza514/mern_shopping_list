875e9bc0d285fe3e303f1b4d981c37744186e426
Juan Meza <jrmeza514@gmail.com>
2021-07-07 22:20:36 -0700

    Basic form for updating the account information
    No api endpoint is setup for this. WIP.

--- client/src/components/pages/AccountPage.tsx
+++ client/src/components/pages/AccountPage.tsx
@@ -1,21 +1,50 @@
-import React from 'react';
+import React, { ChangeEventHandler, FormEvent, useState } from 'react';
 import { connect } from 'react-redux';
-import { IAuthReduxProps } from '../../types/interfaces';
+import { IAuthReduxProps, ITarget, IUserState } from '../../types/interfaces';
+import { IconButton, Paper, Button, Input, Card, TextField } from '@material-ui/core';
+import { Edit as EditIcon, Refresh as ClearIcon } from '@material-ui/icons';
 
 interface AccountPageProps {
-
+    user: IUserState
 }
 
-function AccountPage({ }: AccountPageProps) {
+function AccountPage({ user }: AccountPageProps) {
+
+    const [editDisabled, setEditDisabled] = useState(true);
+    const [userName, setUserName] = useState(user.name);
+    const [userEmail, setUserEmail] = useState(user.email);
+
+    const onNameChange = (e: ITarget) => setUserName(e.target.value);
+    const onEmailChange = (e: ITarget) => setUserEmail(e.target.value);
+    const toggleEditable = () => setEditDisabled(!editDisabled);
+    const resetForm = () => {
+        setUserName(user.name);
+        setUserEmail(user.email);
+    }
+
     return (
         <div>
-
+            <Card style={{ padding: '20px', maxWidth: '600px', margin: 'auto' }}>
+                <IconButton onClick={toggleEditable}>
+                    <EditIcon color="secondary" fontSize="small" />
+                </IconButton>
+                <IconButton onClick={resetForm}>
+                    <ClearIcon color="secondary" fontSize="small" />
+                </IconButton>
+                <form>
+                    <TextField id="name" name="name" label="Name" fullWidth margin="normal"
+                        value={userName} disabled={editDisabled} variant="outlined" onChange={onNameChange} />
+                    <TextField id="email" name="email" type="email" label="Email" fullWidth margin="normal"
+                        value={userEmail} disabled={editDisabled} variant="outlined" onChange={onEmailChange} />
+                    <Button color="secondary" disabled={editDisabled}>Update</Button>
+                </form>
+            </Card>
         </div>
     )
 }
 
-const mapStateToProps = (state: IAuthReduxProps) => {
-
-}
+const mapStateToProps = (state: IAuthReduxProps) => ({
+    user: state.auth.user
+})
 
 export default connect(mapStateToProps, {})(AccountPage);
